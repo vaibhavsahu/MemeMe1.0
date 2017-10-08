5bc937fb2ef1fc7b4cc7305b9c0a165a90cae5f1
vaibhav sahu <vaibh3jec@gmail.com>
2017-10-08 12:59:10 -0600

    Added comments for functions

--- MemeMe2.0/ViewController.swift
+++ MemeMe2.0/ViewController.swift
@@ -11,15 +11,12 @@ import AVFoundation
 import Photos
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
-    
+    //Meme text attributes
     let memeTextAttributes:[String:Any] = [
         NSAttributedStringKey.strokeColor.rawValue: UIColor.black,
         NSAttributedStringKey.foregroundColor.rawValue: UIColor.white,
         NSAttributedStringKey.font.rawValue: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
-    
-    NSAttributedStringKey.strokeWidth.rawValue: -3.0
-        //NSAttributedStringKey.
-        
+        NSAttributedStringKey.strokeWidth.rawValue: -3.0
     ]
     @IBOutlet weak var cameraButton: UIBarButtonItem!
     @IBOutlet weak var imageView: UIImageView!
@@ -46,6 +43,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         topTextField.text! = "TOP"
         bottomTextField.text! = "BOTTOM"
         
+        //setting defualt attributes for textfields
         topTextField.defaultTextAttributes = memeTextAttributes
         bottomTextField.defaultTextAttributes = memeTextAttributes
         
@@ -54,67 +52,63 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         //initially set the share button to disabled state
         shareButton.isEnabled = false
-        
         subscribeToKeyboardNotifications()
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        //if it is a simulator, camera button should be disabled else enabled
         cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
-        
-        //if image is available in Meme Editor, sharebutton will be enabled else it is set to be disabled
-        
-        //subscribeToKeyboardNotifications()
         unsubscribeFromKeyboardNotifications()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
-        
         super.viewWillDisappear(animated)
-        
-    }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
   
-    
+    //function: add observers to keyboard events
     func subscribeToKeyboardNotifications() {
         
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: .UIKeyboardWillShow, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: .UIKeyboardWillShow, object: nil)
 
     }
-    
+    //function: removes added observers to keyboard
     func unsubscribeFromKeyboardNotifications() {
         
         NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillShow, object: nil)
         NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
     }
     
+    //keyboard show function
     @objc func keyboardWillShow(_ notification:NSNotification) {
         self.view.frame.origin.y -= getKeyboardHeight(notification as Notification)
     }
     
+    //keyboard hide function
     @objc func keyboardWillHide(_ notification: NSNotification) {
-        self.view.frame.origin.y += getKeyboardHeight(notification as Notification)
+        self.view.frame.origin.y = 0
     }
     
+    //this function would return the keyboard height; this function would be used keyboardShow/Hide functions
     func getKeyboardHeight(_ notification:Notification) -> CGFloat {
         
         let userInfo = notification.userInfo
         let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue // of CGRect
         return keyboardSize.cgRectValue.height
     }
+    
+    //keyboard begin editing delegate method
     func textFieldDidBeginEditing(_ textField: UITextField) {
         //clear the text as soon as users begin editing
         textField.text! = ""
        // bottomTextField.text! = ""
     }
     
+    //keyboard return key delegate method
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
-        self.view.frame.origin.y = 0
+        //self.view.frame.origin.y = 0
         return true
     }
     //user selects the image picker controller
@@ -150,7 +144,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         return memedImage
     }
     
-    
+    //function: Share action method
     @IBAction func shareActivityAction(_ sender: Any) {
         //generate a memed image
         let memedImage: UIImage = generateMemedImage()
@@ -160,7 +154,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         activityViewController.popoverPresentationController?.sourceView = self.view
         activityViewController.completionWithItemsHandler = { (activity, success, items, error) in
             if(success && error == nil){
-                //Do Work
                 self.save()
                 self.dismiss(animated:true, completion: nil);
             }else if (error != nil){
@@ -174,17 +167,17 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         dismiss(animated: true, completion: nil)
     }
-
+    
+    //function: select source as camera to capture pics/memes
     @IBAction func pickImageFromCamera(_ sender: Any) {
         let imagePicker = UIImagePickerController()
         imagePicker.sourceType = .camera
         imagePicker.delegate = self
         present(imagePicker, animated: true , completion: nil)
         _ = UIImagePickerController.isSourceTypeAvailable(.camera)
-        
-        //print(UIImagePickerController.availableMediaTypes(for: .camera) as Any)
     }
     
+    //function: select source as photo album for memes
     @IBAction func pickImageFromAlbum(_ sender: Any) {
         let imagePicker = UIImagePickerController()
         imagePicker.sourceType = .photoLibrary
